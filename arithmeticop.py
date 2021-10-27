4b0353d35a03c5e316254f8eb16a19e2d2d615e2
alas1413 <92256695+alas1413@users.noreply.github.com>
2021-10-27 09:14:04 +0530

    Add files via upload

---
+++ arithmeticop.py
@@ -0,0 +1,26 @@
+print(10+3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+x = 10
+x += 3
+print(x)
+
+x= 20
+x -= 3
+print(x)
+
+# operator precedence
+x = 10 + 3* 6
+print(x)
+
+# par, exp , mul/div , add/sub
+x = (2 + 3) * 10 - 3
+
+# math fuctions
+Y = 2.9
+print(round(Y))
+print(abs(-2.9))
+
+
+
