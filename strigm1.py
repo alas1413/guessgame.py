70cdeb90146ba62376a517c0fa36d1bc0b565848
alas1413 <92256695+alas1413@users.noreply.github.com>
2021-10-27 09:17:21 +0530

    Add files via upload

---
+++ strigm1.py
@@ -0,0 +1,11 @@
+msg = 'these are the string methods'
+print(msg)
+print(len(msg))
+print(msg.upper())
+print(msg.lower())
+print(msg.title())
+print(msg.find('a'))
+print(msg.replace( 'method','types'))
+
+
+
